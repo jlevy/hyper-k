1c1b5ea779d05140238bed28f2346f1f3fa64bb0
Joshua Levy <joshua@cal.berkeley.edu>
2024-11-10 09:49:26 -0800

    Nicer tooltip transitions.

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -8,31 +8,54 @@ const tooltipStyle = {
   padding: "5px",
   pointerEvents: "none",
   zIndex: 1000,
-  transition: "opacity 0.3s ease-in-out, visibility 0.3s ease-in-out",
+  transition: "opacity 0.2s ease-in, visibility 0.2s ease-in",
   opacity: 0,
   visibility: "hidden",
 };
 
 class Tooltip extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      visiblePosition: props.position,
+      transitioning: false,
+    };
+  }
+
+  componentDidUpdate(prevProps) {
+    // Transition more smoothly from one position to another.
+    if (prevProps.position !== this.props.position) {
+      // Start fade out
+      this.setState({ transitioning: true });
+
+      // Update position after fade out
+      setTimeout(() => {
+        this.setState({
+          visiblePosition: this.props.position,
+          transitioning: false,
+        });
+      }, 200);
+    }
+  }
+
   render() {
-    const { visible, content, position, fontSize } = this.props;
+    const { visible, content, fontSize } = this.props;
+    const { visiblePosition, transitioning } = this.state;
 
     const VIEWPORT_HEIGHT = window.innerHeight;
     const TOP_THRESHOLD = VIEWPORT_HEIGHT * 0.1; // Top 10% of screen
     const HORIZONTAL_OFFSET = 10;
     const VERTICAL_OFFSET = 25;
 
-    // Default to above and right of cursor/element.
-    // If we are near the top of the screen, position below instead.
     const verticalOffset =
-      position.y < TOP_THRESHOLD ? VERTICAL_OFFSET : -VERTICAL_OFFSET;
+      visiblePosition.y < TOP_THRESHOLD ? VERTICAL_OFFSET : -VERTICAL_OFFSET;
 
     const style = {
       ...tooltipStyle,
       fontSize: fontSize,
-      left: position.x + HORIZONTAL_OFFSET,
-      top: position.y + verticalOffset,
-      opacity: visible ? 1 : 0,
+      left: visiblePosition.x + HORIZONTAL_OFFSET,
+      top: visiblePosition.y + verticalOffset,
+      opacity: visible && !transitioning ? 1 : 0,
       visibility: visible ? "visible" : "hidden",
     };
 
